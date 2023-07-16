5fbd444684c214aa903f4a1a5bf9623abe50c309
TimotHome <TimoGut@yahoo.com>
2023-07-16 21:56:27 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,97 @@
+// express runs the server
+import express from "express"
+//Cross-Origin Resource Sharing 
+import cors from "cors"
+import fs, { ObjectEncodingOptions } from "fs"
+import path from "path"
+import bodyParser from "body-parser"
+
+StartServer(3654,"C:");
+
+function StartServer(PORT:number,PUBLICFOLDER:string){
+    const WEBSITE_PREFIX = "http://"
+
+    //create the api
+    const app = express() 
+
+    const corsOptions ={
+        origin:'*', 
+        credentials:true,            //access-control-allow-credentials:true
+        optionSuccessStatus:200,
+    }
+
+    //Middleware
+    app.use(cors(corsOptions))
+    app.use(express.static(PUBLICFOLDER));
+
+    app.use(express.json())
+    app.use(bodyParser.urlencoded({extended:false}))
+
+    //CRUD: Create Read Update Delete
+
+    app.get("*", (req, res, next) => {
+        if(req.url === "/favicon.ico")
+        {
+            res.send()
+            return;
+        }
+        
+        let host = req.headers.host;
+        let url = req.url;
+
+        let text = "Server is online!<br>";
+
+        //from "/Users/Timo/" to "/Users/"
+        let splitUrl = url.split("/")
+        let parentURL = url.replace(splitUrl[splitUrl.length - 2], "") // remove the last /*path*/  from "/Users/Timo/" to "/Users//"
+        if(parentURL.endsWith("//")){
+            parentURL = parentURL.slice(0,parentURL.length-2) //removes the last  slash "//" => "/" from "/Users//" to "/Users/"
+        }
+        
+
+        if(url != "/"){
+            
+            let returnLink = "<a href=\"" + WEBSITE_PREFIX + host + parentURL + "\">return</a> <br>";
+            text += returnLink;
+        }
+
+        let allFiles = getAllFiles(PUBLICFOLDER + url);
+
+        
+
+        allFiles.forEach(file => {
+            text += "<br><a href=\"" + WEBSITE_PREFIX + host  + url +  file + "\">" + file + "</a>";
+        });
+        res.send(text)
+        
+        
+    })
+
+    function getAllFiles(localPath:string) : string[]{
+        //let globalPath = path.join(__dirname,localPath) 
+        try{
+            let files = fs.readdirSync(localPath);
+            //listing all files using forEach
+            
+            files?.forEach(function (file) {
+                // Do whatever you want to do with the file
+                console.log("File:" + file); 
+                
+            });
+            return files;
+        }catch(err){
+            console.log("error finding " + localPath)
+        }
+
+
+
+        
+
+        
+    }
+
+
+    app.listen(PORT,() => {
+        console.log("[Server]: Server is running at http://localhost:" + PORT)
+    })
+}
